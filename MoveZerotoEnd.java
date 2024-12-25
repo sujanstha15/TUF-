444be49de81db94c69764f0469f4a1412aa2e72c
sujanstha15 <raju40rr@gmail.com>
2024-12-24 22:25:39 -0600

    program to move 0's to end

---
+++ MoveZerotoEnd.java
@@ -0,0 +1,17 @@
+public class MoveZerotoEnd {
+    class Solution {
+        public void moveZeroes(int[] nums) {
+            int insertPosition=0;
+            for(int i=0; i<nums.length; i++){
+                //fill all non zero numbers
+                if(nums[i]!=0){
+                    nums[insertPosition] = nums[i];
+                    insertPosition++;
+                }
+            }
+            while(insertPosition<nums.length){
+                nums[insertPosition++]=0;
+            }
+        }
+    }
+}
