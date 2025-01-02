2fa294063f71dc62904ed32b3522e51d9137553a
sujanstha15 <raju40rr@gmail.com>
2025-01-02 13:14:08 -0600

    solution

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -1,3 +1,25 @@
 public class SearchInsertPosition {
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+         int low=0;
+         int high = nums.length-1;
+         //looping through the array
+         while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+            
+         } 
+         return low;
+        }
+        
+    }
     
 }
