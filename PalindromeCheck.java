53324f0b64e814ff9a06a4904327b8372af7deaf
sujanstha15 <raju40rr@gmail.com>
2025-01-21 22:13:09 -0600

    palindrome checker

---
+++ PalindromeCheck.java
@@ -0,0 +1,12 @@
+public class PalindromeCheck {
+    public Boolean isPalindrome(String str)
+	{
+		String reverse = "";
+		for(int i=str.length()-1; i>=0; i--)
+			reverse = reverse + str.charAt(i);
+		if(str.equals(reverse))
+			return true;
+		else
+			return false;
+	}
+}
